00b0878acac09cd4c361f62c99684c76b082ba87
SURENA-V <surenahumanoidv@gmail.com>
2022-04-18 19:01:08 +0430

    gaussian estimator was added.

--- trajectory_planner/src/Estimator.cpp
+++ trajectory_planner/src/Estimator.cpp
@@ -12,3 +12,15 @@ void Estimator::poseVelEstimator(Vector3d &base_vel, Vector3d &base_pos, Vector3
     base_vel += dt_ * (acc - Vector3d(-0.06331, 0.2084210, 10.0047));
     base_pos += dt_ * base_vel;
 }
+
+void Estimator::gaussianPredict(Vector3d &posterior, double &P, double Q, Vector3d delta){
+    posterior += delta;
+    P += Q;
+}
+
+void Estimator::gaussianUpdate(Vector3d &prior, double &P, Vector3d z, double R){
+    Vector3d y = z - prior;
+    double K = P / (P + R);
+    prior += K * y;
+    P = (1 - K) * P;
+}

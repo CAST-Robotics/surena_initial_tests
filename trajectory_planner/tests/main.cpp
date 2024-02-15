fc9d1e93b58ebb35434e8218093692f4d50940f7
amirhosein-vedadi <amirhsein.vedadi@gmail.com>
2024-02-15 18:04:21 +0330

    correct online dcm generation

--- trajectory_planner/tests/main.cpp
+++ trajectory_planner/tests/main.cpp
@@ -44,7 +44,7 @@ int main()
 
     generateStraightFootStep(ankle_foot_steps, base_foot_steps, 0.0, 0.17, 0.0, step_count, 0.0);
 
-    DCMPlanner base_planner(0.68, 1, 0.1, 0.005, step_count + 2, 0.44, 0.01);
+    DCMPlanner base_planner(0.68, 1, 0.1, 0.005, step_count + 2, 0.44, 0.0);
     base_planner.setOnlineFoot(base_foot_steps, 1);
 
     int length = base_planner.getLength();
